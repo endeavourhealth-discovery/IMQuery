5491f9aff50fb7a9120dbdffb9bb22af98ac1199
sheikz13 <contact@appindex.io>
2021-12-23 18:04:25 +0000

    bugs with duplicates solved - now documents are an exact match of dev-test2

--- oss/python-scripts/update.py
+++ oss/python-scripts/update.py
@@ -22,7 +22,7 @@ def updateDocumentsHTTP(index, filename, body):
     data = res.read()
     # writeFile("oss response " + filename, body)
     # print("HTTP Response:" + data.decode("utf-8"))
-    print("doc indexed on  OpenSearch server: " + filename)
+    print("doc indexed on OpenSearch server: " + filename)
 
 
 def updateOSSFromFolder(folderpath=None, index="im-test1"):
@@ -47,11 +47,10 @@ def updateOSSFromFolder(folderpath=None, index="im-test1"):
 
 def writeFile(outputFolderName, filename, content):
 
-
     # Check whether the specified path exists or not
-    outputFolderPath = 'output-json/' + outputFolderName
+    outputFolderPath = 'output-bulk/' + outputFolderName
     if not os.path.exists(outputFolderPath):
-        # Create a new directory because it does not exist 
+        # Create a new directory because it does not exist
         os.makedirs(outputFolderPath)
 
     with open(outputFolderPath + '/' + filename, "w", encoding="utf-8") as f:
@@ -62,21 +61,22 @@ def writeFile(outputFolderName, filename, content):
 def generateDocsFromMySQL(total_rows, saveFile, updateServer, databaseName, index, outputFolderName):
     # established connection between your database
     mysqldb = mysql.connector.connect(
-        host=os.environ.get("mysql_url"), port=int(os.environ.get("mysql_port")), user=os.environ.get("mysql_username"), password=os.environ.get("mysql_password"), database=databaseName)
+        host=os.environ.get("mysql_url"),
+        port=int(os.environ.get("mysql_port")),
+        user=os.environ.get("mysql_username"),
+        password=os.environ.get("mysql_password"),
+        database=databaseName)
 
     mycursor = mysqldb.cursor()
 
-    #try:
-
+    # try:
     max_dbid = total_rows
     increment = 1000
     prev_dbid = 0
     current_dbid = increment
+    total_uploads = 0
 
     while current_dbid <= total_rows:
-
-        if current_dbid == max_dbid:
-            break
 
         statement = """SELECT e.dbid, e.iri, e.name, e.description, e.code, et.type, typ.name, e.scheme, n.name, e.status, stt.name 
                     FROM entity e 
@@ -90,52 +90,81 @@ def generateDocsFromMySQL(total_rows, saveFile, updateServer, databaseName, inde
         mycursor.execute(statement)
         results = mycursor.fetchall()  # fetches all the rows in a result set
 
-        # print(results)
-        # print(len(results))
-
         # convert results to .txt document containing JSON objects (for opensearch BULK api)
         documents = ""
-        dbids = []
+        dbids_skip = []
 
-        for i in results:
-            dbid = i[0]
-            iri = i[1]
-            name = i[2]
-            description = i[3]
-            code = i[4]
-            entityType = i[5]
-            entityTypeName = i[6]
-            scheme = i[7]
-            schemeName = i[8]
-            status = i[9]
-            statusName = i[10]
+        for i in range(0, len(results) - 1):
 
-            # avoids duplicates
-            if (dbid not in dbids):
-                dbids.append(dbid)
+            dbid = results[i][0]
+            iri = results[i][1]
+            name = results[i][2]
+            description = results[i][3]
+            code = results[i][4]
+            entityType = results[i][5]
+            entityTypeName = results[i][6]
+            scheme = results[i][7]
+            schemeName = results[i][8]
+            status = results[i][9]
+            statusName = results[i][10]
 
-                searchDoc = json.dumps({
-                    "iri": iri,
-                    "name": name,
-                    "description": description,
-                    "code": code,
-                    "entityType": entityType,
-                    "entityTypeName": entityTypeName,
-                    "scheme": scheme,
-                    "schemeName": schemeName,
-                    "status": status,
-                    "statusName": statusName
-                })
+            # skips for loop if dbid is to be skipped (e.g. for multiple joins)
+            if (dbid in dbids_skip):
+                continue
 
-                # opensearch operation
-                documents = documents + \
-                    '{ "index": { "_index": "' + index + '", "_id": "' + \
-                    str(dbid) + '" } }' + '\n'
-                # docuemnt
-                documents = str(documents) + str(searchDoc) + "\n"
+            entityTypes = [{
+                "@id": entityType,
+                "name": entityTypeName
+            }]
+
+            current_index = i
+            # print(len(results))
+            # print(current_index)
+            # print(results[current_index + 1][0])
+            # print(dbids_skip)
+            while current_index < len(results) -1:
+                if results[current_index][0] == results[current_index + 1][0]:
+                    entityTypes.append(
+                        {
+                            "@id": results[current_index + 1][5],
+                            "name": results[current_index + 1][6]
+                        }
+                    )
+                    dbids_skip.append(results[current_index + 1][0])
+                    current_index += 1
+                else:
+                    break
+                    # dbids_skip.append(current_index + 1)
+
+
+            searchDoc = {
+                "iri": iri,
+                "name": name,
+                "description": description,
+                "code": code,
+                "entityType": entityTypes,
+                "scheme": {
+                    "@id": scheme,
+                    "name": schemeName
+                },
+                "status": {
+                    "@id": status,
+                    "name": statusName
+                },
+            }
+            searchDocJSON = json.dumps(searchDoc)
+
+            # opensearch operation
+            documents = documents + \
+                '{ "index": { "_index": "' + index + '", "_id": "' + \
+                str(dbid) + '" } }' + '\n'
+            # docuemnt
+            documents = str(documents) + str(searchDoc) + "\n"
+
+            total_uploads += 1
 
         # write document to file
-        filename = str(prev_dbid) + "-" + str(current_dbid) + ".txt"
+        filename = str(prev_dbid + 1) + "-" + str(current_dbid) + ".txt"
 
         if saveFile:
             writeFile(outputFolderName, "file " + filename, documents)
@@ -145,13 +174,16 @@ def generateDocsFromMySQL(total_rows, saveFile, updateServer, databaseName, inde
             updateDocumentsHTTP(index, filename, documents)
 
         # increment current_dbid
-        if (current_dbid + increment > total_rows):
+        if current_dbid == max_dbid:
+            break
+            print('update complete - total docs added: ' + total_uploads)
+        elif (current_dbid + increment > total_rows):
             prev_dbid = current_dbid
             current_dbid = max_dbid
         else:
             prev_dbid = current_dbid
             current_dbid += increment
-    
+
     mysqldb.close()
 
     # except:
@@ -162,5 +194,10 @@ def generateDocsFromMySQL(total_rows, saveFile, updateServer, databaseName, inde
 
 if __name__ == '__main__':
     print('updating opensearch has started')
-    generateDocsFromMySQL(databaseName="im3", index="im-test1", outputFolderName="im-test1", total_rows=2659160, saveFile=True, updateServer=True)
-    print('update complete')
+    generateDocsFromMySQL(
+        databaseName="im3",
+        index="im-test2",
+        outputFolderName="im-test2",
+        total_rows=2659160,
+        saveFile=True,
+        updateServer=True)
