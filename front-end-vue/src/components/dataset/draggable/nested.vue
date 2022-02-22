12bb183390eef564fdd4365101e35c9a11e652f2
sheikz13 <contact@appindex.io>
2022-02-22 16:25:35 +0000

    dragging and nesting improved

--- front-end-vue/src/components/dataset/draggable/nested.vue
+++ front-end-vue/src/components/dataset/draggable/nested.vue
@@ -7,25 +7,61 @@
       </li>
   </draggable>
 </template> -->
-<template>
+<template @mouseleave="isCardDragged = false">
   <draggable
     :list="tasks"
     item-key="name"
-    class="list-group dragArea"
-    ghost-class="ghost"
+    :class="
+      'select-none cursor-pointer dragArea rounded-sm order-color-300 ' +
+        [isCardDragged ? ' ' : '']
+    "
+    ghost-class="bg-blue-300"
     :group="{ name: 'g1' }"
-    tag="ul"
-
+    tag="div"
+    h="auto"
+    @drag="isCardDragged = true"
+    @dragend="isCardDragged = false"
+    @drop="isCardDragged = false"
+    @mouseleave="isCardDragged = false"
   >
-    <template #item="{ element }">
-      <li class="list-group-item">
-        <p class="select-none cursor-pointer">
-          {{ element.name }}
-        </p>
-        <div>
-          Content
+    <template #item="{ element, index }">
+      <div ref="listGroup" class="">
+        <div
+          v-if="index == 0"
+          class="select-none cursor-pointer text-gray-700 font-medium px-2"
+        >
+          Include a person if they match all descriptions
         </div>
-        <nested-draggable class="ml-5" :tasks="element.tasks" item-key="name" />
+        <div
+          class="select-none cursor-pointer text-black font-bold bg-white rounded-sm mx-5 my-1 px-2 py-1"
+        >
+          {{ element.name }}
+        </div>
+        <nested-draggable
+          :class="
+            'ml-5 dragArea__children' +
+              [
+                isCardDragged && !element.tasks.length
+                  ? ' min-h bg-blue-100'
+                  : '',
+              ] +
+              [!isCardDragged && !element.tasks.length ? ' hidden' : ' ']
+          "
+          :tasks="element.tasks"
+          item-key="name"
+          @drag="isCardDragged = true"
+          @dragend="isCardDragged = false"
+          @drop="isCardDragged = false"
+        />
+
+        <!-- <nested-draggable
+          v-else
+          :class="
+            'mt-10 ml-10 bg-gray-300 dragArea__nochildren' +
+              [isCardDragged ? ' min-h' : ' hidden']
+          "
+        /> -->
+
         <!-- <nested-draggable
           v-if="element.tasks.length"
           class="ml-5"
@@ -38,7 +74,7 @@
           :tasks="element.tasks"
           item-key="name"
         /> -->
-      </li>
+      </div>
     </template>
   </draggable>
 </template>
@@ -48,9 +84,7 @@ import draggable from "vuedraggable";
 import { ref, onMounted, defineComponent } from "vue";
 
 export default defineComponent({
-  props: {
-    tasks: [] as any,
-  },
+  props: ["tasks", "isParentDragged"],
   components: {
     draggable,
   },
@@ -58,16 +92,38 @@ export default defineComponent({
   name: "nested-draggable",
   data() {
     return {
-      onDrag: false,
+      // isDrag: false,
     };
+  },
+  computed: {
+    isCardDragged: {
+      get(): boolean {
+        return this.$store.state.isCardDragged;
+      },
+      set(val: boolean): void {
+        this.$store.commit("updateIsCardDragged", val);
+      },
+    },
   },
 });
 </script>
 <style scoped>
 .dragArea {
-  min-height: 50px;
+  /* max-height: 800px; */
+  /* overflow-y: visible; */
+  /* display: absolute; */
   min-width: 200px;
   outline: 1px solid;
+}
+
+.dragArea__nochildren {
+}
+
+.min-h {
+  min-height: 25px;
+  height: 25px;
+  /* min-width: 200px; */
+  /* width: 200px; */
 }
 
 .non-selectable {
@@ -75,5 +131,29 @@ export default defineComponent({
   -moz-user-select: none; /* Firefox all */
   -ms-user-select: none; /* IE 10+ */
   user-select: none; /* Likely future */
+}
+
+/* Scrollbar */
+/* width */
+::-webkit-scrollbar {
+  width: 0;
+  height: 0;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #ffffff;
+  /* darker colour: #f1f1f1; */
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  border-radius: 5px;
+  background: rgba(136, 136, 136, 0.233);
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #555;
 }
 </style>
